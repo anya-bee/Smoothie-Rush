f677faec1bb6760d8a99eb4e1f1baad08eb25a23
Andrea Salazar <andrea.salazar.lizardo@gmail.com>
2023-03-12 01:02:33 -0600

    Changes between scenes and inventory slot test

---
+++ Smoothie-Rush-T1/Assets/Script/Draggable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour
+{
+
+    Vector3 mousePositionOffset;
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        //capture mouse posiiton and return WorldPoint
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+
+    private void OnMouseDown()
+    {
+        // capture mouse offset 
+        mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + mousePositionOffset; 
+    }
+}
